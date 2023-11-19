a9802794f1eda00f3958c86493a1ca415bdf5fbd
pTrautner <150627551+pTrautner@users.noreply.github.com>
2023-11-19 12:28:00 +0100

    new UE: UE6.6

--- UE6.6/Program.cs
+++ UE6.6/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using UE66_ship;
+namespace UE66_Main;
+
+class Program
+{
+    static public void Main(string[] args)
+    {
+        Ship s1 = new Ship("skipper1", 2005, 12);
+        Ship s2 = new Ship("flipper2", 1999, 17);
+        Ship s3 = new Ship("shipper3", 2000, 15);
+        Ship s4 = new Ship("tripper4", 2001, 17);
+        Ship s5 = new Ship("clipper5", 2002, 15);
+        List<Ship> shiplist = new List<Ship>() { s1, s2, s3, s4, s5 };
+        DeleteShip("shipper3", shiplist);
+
+
+    }
+    static void DeleteShip(string shipname, List<Ship> shiplist)
+    {
+        int indexofdeletion = 0;
+        foreach (Ship ship in shiplist) { Console.WriteLine(ship._name); }
+        foreach (Ship s in shiplist)
+        {
+            if (s._name == "shipper3")
+            {
+                break;
+            }
+            indexofdeletion++;
+        }
+        shiplist.RemoveAt(indexofdeletion);
+        Console.WriteLine("new list:");
+        foreach (Ship ship in shiplist) { Console.WriteLine(ship._name); }
+    }
+}
---
+++ UE6.6/Ships.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UE66_ship
+{
+    internal class Ship : IComparable<Ship>
+    {
+        public string _name;
+        public int _baujahr;
+        public decimal _laenge;
+        public Ship(string name, int baujahr, decimal laenge)
+        {
+            this._name = name;
+            this._baujahr = baujahr;
+            this._laenge = laenge;
+        }
+        public string? Name { get; set; }
+        //public int Baujahr { get; set; }
+        public int GetBaujahr() { return _baujahr; }
+        public void SetBaujahr(int baujahr) { _baujahr=baujahr; }
+        public decimal Laenge { get; set; }
+        public int CompareTo(Ship other)
+        {
+
+            int lengthComparison = this._laenge.CompareTo(other._laenge);
+            return lengthComparison;
+        }
+    }
+}
