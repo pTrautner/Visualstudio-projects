25112318b6054bf0793056fdc2b174fc77601a66
pTrautner <150627551+pTrautner@users.noreply.github.com>
2023-11-25 14:08:59 +0100

    7.5 nesting stuff improved

--- UE07_05/Program.cs
+++ UE07_05/Program.cs
@@ -14,34 +14,31 @@ public class Program
                 int enteredKontoNumber = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("enter pin");
                 int enteredpin = Convert.ToInt32(Console.ReadLine());
-                bool pinMatches = b1.PinAndKontoCheck(enteredpin, enteredKontoNumber);
-                if (pinMatches == true)
+                bool userloggedin;
+                userloggedin = b1.PinAndKontoCheck(enteredpin, enteredKontoNumber);
+                while (userloggedin == true)
                 {
-                    bool userloggedin = true;
-                    while (userloggedin == true)
+                    Console.WriteLine($"Hi {b1.Kontoinhaber}! Current Balance: {b1.Kontostand}");
+                    Console.WriteLine("What would you like to do? Deposit [1] Withdraw [2] Logout [3] Exit App [4]");
+                    int userchoice = Convert.ToInt32(Console.ReadLine());
+                    if (userchoice < 1 || userchoice > 4)
                     {
-                        Console.WriteLine($"Hi {b1.Kontoinhaber}! Current Balance: {b1.Kontostand}");
-                        Console.WriteLine("What would you like to do? Deposit [1] Withdraw [2] Logout [3] Exit App [4]");
-                        int userchoice = Convert.ToInt32(Console.ReadLine());
-                        if (userchoice < 1 || userchoice > 4)
-                        {
-                            Console.WriteLine("invalid selection!");
-                        }
-                        switch (userchoice)
-                        {
-                            case 1:
-                                b1.Deposit();
-                                continue;
-                            case 2:
-                                b1.Withdraw();
-                                continue;
-                            case 3:
-                                userloggedin = false;
-                                break;
-                            case 4:
-                                Environment.Exit(0);
-                                return;
-                        }
+                        Console.WriteLine("invalid selection!");
+                    }
+                    switch (userchoice)
+                    {
+                        case 1:
+                            b1.Deposit();
+                            continue;
+                        case 2:
+                            b1.Withdraw();
+                            continue;
+                        case 3:
+                            userloggedin = false;
+                            break;
+                        case 4:
+                            Environment.Exit(0);
+                            return;
                     }
                 }
             }
