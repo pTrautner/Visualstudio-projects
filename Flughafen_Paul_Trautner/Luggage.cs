ac477bd0da0f90f368908d85955fed9c3dc04268
pTrautner <150627551+pTrautner@users.noreply.github.com>
2023-11-25 15:28:26 +0100

    flughafen: making basic classes
    luggage inheriting stuff

---
+++ Flughafen_Paul_Trautner/Luggage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTluggage;
+
+class Luggage
+{
+    //Constructor
+    public Luggage(string name, string flightnumber, double weight)
+    {
+        this.Name = name;
+        this.Flightnumber = flightnumber;
+        this.Weight = weight;
+    }
+    public string Name { get; set; }
+    public string Flightnumber { get; set;}
+    public double Weight { get; set; }
+}
+
+class Handluggage : Luggage
+{
+    Handluggage(string name, string flightnumber, double weight) : base(name, flightnumber, weight)
+    {
+        this.Weight = base.Weight + 8;
+    }
+}
