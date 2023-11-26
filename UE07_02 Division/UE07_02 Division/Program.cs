25b09d2e0ad7b8c15cf3335c30f5d09436d46b08
pTrautner <150627551+pTrautner@users.noreply.github.com>
2023-11-26 12:49:41 +0100

    rechner

---
+++ UE07_02 Division/UE07_02 Division/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace UE07_02_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try     // Der Code innerhalb des try-Blocks enthält den potenziell fehleranfälligen Code
+                {      // Wenn eine Exception auftritt, wird die normale Ausführung des Codes abgebrochen und die Kontrolle wird zum entsprechenden catch-Block weitergeleitet
+                       // Benutzereingabe
+                    Console.Write("Geben Sie die erste Ganzzahl ein: ");
+                    int zahl1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Geben Sie die zweite Ganzzahl ein: ");
+                    int zahl2 = Convert.ToInt32(Console.ReadLine());
+
+                    // Division mittels Funktion durchführen
+                    int ergebnis = Divide(zahl1, zahl2);
+
+                    // Ergebnis ausgeben
+                    Console.WriteLine($"Ergebnis der Division: {ergebnis}");
+                }
+                catch (FormatException ex) // FormatException ist eine vordefinierte Exception-Klasse in C#
+                {
+                    // Fehler bei der Konvertierung der Eingabe zu Int
+                    Console.WriteLine($"Fehler: Ungültige Eingabe. {ex.Message}"); // Dem System schon bekannte Nachricht wird geschrieben
+                }
+                catch (DivideByZeroException ex) // vordefinierte Exception-Klasse
+                {
+                    // Fehler bei Division durch Null
+                    Console.WriteLine($"Fehler: Division durch Null ist nicht erlaubt. {ex.Message}");
+                }
+                catch (Exception ex) // Allgemeiner Ausnahmefall
+                {
+                    Console.WriteLine($"Ein Fehler ist aufgetreten. {ex.Message}");
+                }
+                finally // Diser Code wird immer ausgeführt
+                {
+                    Console.WriteLine("Vielen Dank für die Nutzung des Divisionsrechners!");
+                }
+            }
+            
+        }
+        static int Divide(int zähler, int nenner) // Statische Funktion können durch ihre Name referenziert werden
+        {
+            return zähler / nenner;
+        }
+    } // Wenn sich der Datentyp der eingegebenen Zahlen ändert: Wenn der Datentyp nicht mit der erwarteten Konvertierung übereinstimmt, wird eine FormatException ausgelöst
+}    // Dies kann geschehen, wenn der Benutzer Buchstaben oder Sonderzeichen anstelle von Zahlen eingibt
---
+++ UE07_04 Taschenrechner/UE07_04 Taschenrechner/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace UE07_04_Taschenrechner
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try    // Der Code innerhalb des try-Blocks enthält den potenziell fehleranfälligen Code
+            {     // Wenn eine Exception auftritt, wird die normale Ausführung des Codes abgebrochen und die Kontrolle wird zum entsprechenden catch-Block weitergeleitet
+                // Benutzereingabe
+                Console.Write("Geben Sie die erste Zahl ein: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Geben Sie die zweite Zahl ein: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                // Operator wählen
+                Console.Write("Geben Sie einen der Grundoperatoren (+, -, *, /) ein: ");
+                char operation = Convert.ToChar(Console.ReadLine());
+
+                // Funktion aufrufen und Berechnen
+                double result = PerformOperation(num1, num2, operation);
+                Console.WriteLine($"Ergebnis der Berechnung: {result}");
+            }
+            catch (FormatException ex) // FormatException ist eine vordefinierte Exception-Klasse in C#
+            {
+                // Fehler bei der Konvertierung der Eingabe zu Double
+                Console.WriteLine($"Fehler: Ungültige Eingabe. {ex.Message}");
+            }
+            catch (DivideByZeroException ex) // vordefinierte Exception-Klasse
+            {
+                // Fehler bei Division durch Null
+                Console.WriteLine($"Fehler: Division durch Null ist nicht erlaubt. {ex.Message}");
+            }
+            catch (Exception ex) // Allgemeiner Ausnahmefall
+            {
+                Console.WriteLine($"Ein Fehler ist aufgetreten. {ex.Message}");
+            }
+            finally // Diser Code wird immer ausgeführt
+            {
+                Console.WriteLine("Vielen Dank für die Nutzung des Taschenrechners!");
+            }
+        }
+
+        // Zwischen vershiedenen Operationen wählen
+        static double PerformOperation(double num1, double num2, char operation)
+        {
+            // Mathematische Operationen als statische Methoden
+            switch (operation)
+            {
+                case '+':
+                    return Add(num1, num2);
+                case '-':
+                    return Subtract(num1, num2);
+                case '*':
+                    return Multiply(num1, num2);
+                case '/':
+                    return Divide(num1, num2);
+                default:
+                    throw new InvalidOperationException("Ungültiger Operator");
+            }
+        }
+
+        static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        static double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        static double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Division durch Null ist nicht erlaubt.");
+            }
+            return num1 / num2;
+        }
+    }
+}
